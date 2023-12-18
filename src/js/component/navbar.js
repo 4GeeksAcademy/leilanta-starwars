e1fc0d249929d242402c30e6ee189d78df05b648
Leilani Tanimoto Abad <135250913+Leilanta@users.noreply.github.com>
2023-12-18 15:47:06 +0000

    layout

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -8,7 +8,7 @@ export const Navbar = () => {
 		<div id="mainNavigation">
 		<nav role="navigation">
 		  <div className="py-3 text-center border-bottom">
-			<img src="https://pngimg.com/uploads/star_wars_logo/star_wars_logo_PNG5.png" alt="" className=" logo invert"/>
+			<img src="https://www.pngall.com/wp-content/uploads/13/Star-Wars-Logo-Transparent.png" alt="" className=" logo invert"/>
 		  </div>
 		</nav>
 		<div className="navbar-expand-md">
@@ -16,22 +16,6 @@ export const Navbar = () => {
 			<button className="navbar-toggler w-75" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
 			  <span className="navbar-toggler-icon"></span> <span className="align-middle">Menu</span>
 			</button>
-		  </div>
-		  <div className="text-center mt-3 collapse navbar-collapse" id="navbarNavDropdown">
-			<ul className="navbar-nav mx-auto ">
-			  <li className="nav-item">
-				<a className="nav-link active" aria-current="page" href="#">Characters</a>
-			  </li>
-			  <li className="nav-item">
-				<a className="nav-link" href="#">Starships</a>
-			  </li>
-			  <li className="nav-item">
-				<a className="nav-link" href="#">Planets</a>
-			  </li>
-			  <li className="nav-item">
-				<a className="nav-link" href="#">Favourites</a>
-			  </li>
-			</ul>
 		  </div>
 		</div>
 	  </div>
